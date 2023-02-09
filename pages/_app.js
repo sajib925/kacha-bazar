1b2ad644f71cd25f2317a38734ff2a933923e15a
sajib925 <sajibahmed1004@gmail.com>
2023-02-09 11:55:20 +0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import { Inter } from '@next/font/google';
+
+const inter = Inter({subsets: ["latin"], variable: ["--font-inter"]});
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
